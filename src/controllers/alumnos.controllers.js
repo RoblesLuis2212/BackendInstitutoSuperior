b1ad19750b87428ee04043faa44fef7567258f23
Luis Robles <luisgeremiasrobles@gmail.com>
2026-01-12 21:49:05 -0300

    Agregue el controlador para listar alumnos por id y para editar

--- src/controllers/alumnos.controllers.js
+++ src/controllers/alumnos.controllers.js
@@ -27,7 +27,6 @@ export const listarAlumnos = async (req, res) => {
 
 export const eliminarAlumno = async (req, res) => {
   try {
-    console.log("El ID recibido es: ", req.params.id);
     const { id } = req.params;
 
     await prisma.alumno.delete({
@@ -41,5 +40,39 @@ export const eliminarAlumno = async (req, res) => {
       return res.status(404).json({ mensaje: "Usuario no encontrado" });
     }
     res.status(500).json({ mensaje: "Ocurrio un error al eliminar el alumno" });
+  }
+};
+
+export const obtenerAlumnoID = async (req, res) => {
+  try {
+    const alumnoBuscado = await prisma.alumno.findUnique({
+      where: { id: Number(req.params.id) },
+    });
+
+    res.status(200).json(alumnoBuscado);
+  } catch (err) {
+    console.error(err);
+    if (err.code === "P2025") {
+      return res.status(404).json({ mensaje: "El alumno buscado no existe!" });
+    }
+    res.status(500).json({ mensaje: "Ocurrio un error al obtener el alumno" });
+  }
+};
+
+export const actualizarDatosAlumno = async (req, res) => {
+  try {
+    await prisma.alumno.update({
+      where: { id: Number(req.params.id) },
+      data: req.body,
+    });
+
+    res
+      .status(200)
+      .json({ mensaje: "Datos del alumno actualizados exitosamente" });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error actualizar los datos del alumno" });
   }
 };
